c481bf13aa5e2098db83af230196d720a35cdd74
bmoore6 <bmoore6@uccs.edu>
2017-11-22 17:03:50 -0700

    working on regen

---
+++ New Horizon 1/Assets/Scripts/Collectables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour {
+
+    [SerializeField]
+    Sprite[] peppermintLeaves;
+
+    [SerializeField]
+    float boostRegen = 0.01f;
+
+    int boostTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Method that will detect when the player collides with
+    /// the regen pickup. Activate the boosted regen timer
+    /// </summary>
+    
+}
